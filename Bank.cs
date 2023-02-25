1bcb595284975ad37f66ca5482353b688e4876c6
Subhash Barela <barelasubhash16@gmail.com>
2023-02-25 17:49:15 +0530

    Create Bank application

---
+++ Bank.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public class Bank
+    {
+        public int Account_Number;
+        public String Banker_Name;
+        public int balance;
+        public String Bank_Name;
+
+        public Bank(int Account_Number, String Banker_Name, int balance, String Bank_Name)
+        {
+
+            this.Account_Number = Account_Number;
+            this.Banker_Name = Banker_Name;
+            this.balance = balance;
+            this.Bank_Name = Bank_Name;
+        }
+
+        public void withdraw(int number)
+        {
+            if (Account_Number == number)
+            {
+                Console.WriteLine("How many money you want to withdraw");
+                int Amount = Convert.ToInt32(Console.ReadLine());
+
+                if (Amount < balance)
+                {
+                    Console.WriteLine("your current balace is :" + balance);
+                    balance = balance - Amount;
+                    Console.WriteLine("After WithDraw your balance is :" + balance);
+                }
+                else
+                {
+                    Console.WriteLine("Insufficiant balance :....");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Currect Account number..");
+            }
+
+        }
+        public void deposit(int number)
+        {
+            if (Account_Number == number)
+            {
+                Console.WriteLine("How many money you want to withdraw");
+                int D_Amount = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Your current balance is :" + balance);
+                balance = balance + D_Amount;
+                Console.WriteLine("After deposit your balance is :" + balance);
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Currect Account number..");
+            }
+        }
+        public void Check_balance(int number)
+        {
+            if (Account_Number == number)
+            {
+                Console.WriteLine("Your current balance is :" + balance);
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Currect Account number..");
+            }
+        }
+        public void displayInfo(int number)
+        {
+            if (Account_Number == number)
+            {
+                Console.WriteLine("Account number is :" + Account_Number);
+                Console.WriteLine("Banker name is :" + Banker_Name);
+                Console.WriteLine("Account balance is :" + balance);
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Currect Account number..");
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank shiva = new Bank(1,"shivlal barela",1000,"boi");
+            Bank vinu = new Bank(2, "vinesh barela", 2000,"bob");
+            
+            String Bank_Name1 = shiva.Bank_Name;
+            Console.WriteLine(Bank_Name1);
+            Console.WriteLine("Enter the bank name :");
+            String B_Name=Console.ReadLine();
+           
+            if(Bank_Name1 == B_Name) { 
+
+            int choice;
+            do
+            {
+                Console.WriteLine("(1): Withdraw Money ..");
+                Console.WriteLine("(2): Deposit  Money ..");
+                Console.WriteLine("(3): Check Your Balance ..");
+                Console.WriteLine("(4): Your Information ..");
+                Console.WriteLine("(*): For exit press 0 ..\n");
+                Console.WriteLine("Enter Your Choice..");
+                Console.WriteLine("\n");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                       
+                        Console.WriteLine("Enter Your Account Number");
+                        int Acc_number = Convert.ToInt32(Console.ReadLine());
+                        shiva.withdraw(Acc_number);
+                        vinu.withdraw(Acc_number);
+                        break;
+
+                  
+                    
+                    case 2:
+                        Console.WriteLine("Enter Your Account Number");
+                        int Acc1_number = Convert.ToInt32(Console.ReadLine());
+                        shiva.deposit(Acc1_number);
+                        vinu.deposit(Acc1_number);
+                        break;
+                  
+                    
+                    case 3:
+                        Console.WriteLine("Enter Your Account Number");
+                        int Acc2_number = Convert.ToInt32(Console.ReadLine());
+
+                        shiva.Check_balance(Acc2_number);
+                        vinu.Check_balance(Acc2_number);
+
+                        break;
+                 
+                    
+                    case 4:
+                        Console.WriteLine("Enter Your Account Number");
+                        int Acc3_number = Convert.ToInt32(Console.ReadLine());
+
+                        shiva.displayInfo(Acc3_number);
+                        vinu.displayInfo(Acc3_number);
+
+                        break;
+                  
+                    default: Console.WriteLine("You Choose invalid option.. Please insert currect option");
+                        break;
+
+                }
+                Console.WriteLine("\n");
+            } while (choice != 0);
+
+            }
+            else
+            {
+                Console.WriteLine("Bank name is invalid..");
+            }
+
+              Console.Read();
+        }
+    }
+}
